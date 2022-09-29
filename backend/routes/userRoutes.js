2673fd833c925254bd30fe715d82beede94f1a22
nnaemekasama <80992877+nnaemekasama@users.noreply.github.com>
2022-09-28 22:06:06 -1100

    User Reg & password encryption

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from "express"
 const router = express.Router()
 import {
   authUser,
+  registerUser,
   getUserProfile
 } from "../controllers/userController.js"
 import { protect } from '../middleware/authMiddleware.js'
@@ -9,5 +10,6 @@ import { protect } from '../middleware/authMiddleware.js'
 
 router.post('/login', authUser)
 router.route("/profile").get(protect, getUserProfile)
+router.route('/').post(registerUser)
 
 export default router
