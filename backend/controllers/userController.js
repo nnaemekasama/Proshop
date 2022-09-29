e42ef0500c50ec985d8a17a2f076dd5667ed00dd
nnaemekasama <80992877+nnaemekasama@users.noreply.github.com>
2022-09-28 21:34:15 -1100

    custom auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,7 @@ import generateToken from '../utils/generateToken.js'
 // @route   POST /api/users/login
 // @access  Public
 
-export const authUser = asyncHandler(async (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
 
   const user = await User.findOne({ email })
@@ -24,3 +24,24 @@ export const authUser = asyncHandler(async (req, res) => {
     throw new Error('Invalid email or password')
   }
 })
+// @desc    Get user  profile
+// @route   GET /api/users/profile
+// @access  Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if(user) {
+        res.json({
+             _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+  })
+
+export { authUser, getUserProfile }
