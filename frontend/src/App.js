1eb48c978e8036e9aadb7a1b019d236ce21566ec
nnaemekasama <80992877+nnaemekasama@users.noreply.github.com>
2022-10-04 06:40:48 -1100

    payment screen & payment method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
 
 
 
@@ -21,6 +22,7 @@ const App = () => {
       <main className="py-3">
         <Container>
           <Routes>
+            <Route path='/payment' element={<PaymentScreen />} />
             <Route path='/shipping' element={<ShippingScreen />} />
             <Route path='/login' element={<LoginScreen />} />
             <Route path='/register' element={<RegisterScreen />} />
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -27,7 +27,7 @@ const ShippingScreen = () => {
         navigate('/payment')
     }
   return (
-    <FormContainer md={10}>
+    <FormContainer>
         <CheckOutSteps step1 step2/>
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler}>
