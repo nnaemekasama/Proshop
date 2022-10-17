4e0c01331c9a208f2b345236512f1a3549a586eb
nnaemekasama <80992877+nnaemekasama@users.noreply.github.com>
2022-10-17 00:08:16 -1100

    admin middleware & get users endpoint

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -24,6 +24,15 @@ const protect = AsyncHandler(async (req, res, next) => {
     res.status(401);
     throw new Error("unAuthorized Access, no token found");
   }
-});
+})
 
-export { protect }
+const admin = (req, res, next) => {
+  if(req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Not authorized as an admin')
+  }
+}
+
+export { protect, admin }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,13 +4,14 @@ import {
   authUser,
   registerUser,
   getUserProfile,
-  updateUserProfile
+  updateUserProfile,
+  getUsers
 } from "../controllers/userController.js"
-import { protect } from '../middleware/authMiddleware.js'
+import { admin, protect } from '../middleware/authMiddleware.js'
 
 
 router.post('/login', authUser)
 router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile)
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 
 export default router
