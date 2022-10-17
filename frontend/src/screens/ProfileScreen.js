23a1c7f2e89b84d4118c0693945fc64058acc71f
nnaemekasama <80992877+nnaemekasama@users.noreply.github.com>
2022-10-16 18:18:37 -1100

    reset user details & orders after logout

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import {  useNavigate } from 'react-router-dom'
 import { Button, Form, Col, Row, Table } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
@@ -10,7 +10,6 @@ import { listMyOrders } from '../actions/orderActions'
 
 
 const ProfileScreen = () => {
-    let location = useLocation()
     let navigate = useNavigate()
 
 
@@ -114,48 +113,49 @@ const ProfileScreen = () => {
             <h2>My Orders</h2>
             {loadingOrders ? <Loader></Loader> : errorOrders ? <Message variant='danger'>
             {errorOrders}</Message> : (
-                          <Table striped bordered hover responsive className='table-sm'>
-                          <thead>
-                            <tr>
-                              <th>ID</th>
-                              <th>DATE</th>
-                              <th>TOTAL</th>
-                              <th>PAID</th>
-                              <th>DELIVERED</th>
-                              <th></th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {orders.map((order) => (
-                              <tr key={order._id}>
-                                <td>{order._id}</td>
-                                <td>{order.createdAt.substring(0, 10)}</td>
-                                <td>{order.totalPrice}</td>
-                                <td>
-                                  {order.isPaid ? (
-                                    order.paidAt.substring(0, 10)
-                                  ) : (
-                                    <i className='fas fa-times' style={{ color: 'red' }}></i>
-                                  )}
-                                </td>
-                                <td>
-                                  {order.isDelivered ? (
-                                    order.deliveredAt.substring(0, 10)
-                                  ) : (
-                                    <i className='fas fa-times' style={{ color: 'red' }}></i>
-                                  )}
-                                </td>
-                                <td>
-                                  <LinkContainer to={`/order/${order._id}`}>
+                <Table striped bordered hover responsive className='table-sm'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>DATE</th>
+                        <th>TOTAL</th>
+                        <th>PAID</th>
+                        <th>DELIVERED</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.map((order) => (
+                        <tr key={order._id}>
+                            <td>{order._id}</td>
+                            <td>{order.createdAt.substring(0, 10)}</td>
+                            <td>{order.totalPrice}</td>
+                            <td>
+                                {order.isPaid ? (
+                                order.paidAt.substring(0, 10)
+                                ) : (
+                                <i className='fas fa-times' style={{ color: 'red' }}></i>
+                                )}
+                            </td>
+                            <td>
+                                {order.isDelivered ? (
+                                order.deliveredAt.substring(0, 10)
+                                ) : (
+                                <i className='fas fa-times' style={{ color: 'red' }}></i>
+                                )}
+                            </td>
+                            <td>
+                                <LinkContainer to={`/order/${order._id}`}>
                                     <Button className='btn-sm' variant='light'>
-                                      Details
+                                        Details
                                     </Button>
-                                  </LinkContainer>
-                                </td>
-                              </tr>
-                            ))}
-                          </tbody>
-                        </Table>
+                                </LinkContainer>
+                            </td>
+                        </tr>
+                    ))}
+
+                </tbody>
+            </Table>
             )}
         </Col>
     </Row>
