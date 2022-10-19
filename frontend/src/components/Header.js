9a1f57d7310d894bab342060236e436a7708afdb
nnaemekasama <80992877+nnaemekasama@users.noreply.github.com>
2022-10-19 01:00:13 -1100

    product list screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -44,7 +44,7 @@ const Header = () => {
             <LinkContainer to='/admin/userlist'>
               <NavDropdown.Item>Users</NavDropdown.Item>
             </LinkContainer>
-            <LinkContainer to='/admin/userlist'>
+            <LinkContainer to='/admin/productlist'>
               <NavDropdown.Item>Products</NavDropdown.Item>
             </LinkContainer>
             <LinkContainer to='/admin/orderlist'>
